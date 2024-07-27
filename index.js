62b0695920cbfedcfe8fde6f94e666d94201698e
Lauren Coates <lspassey@gmail.com>
2024-07-27 10:41:33 -0700

    finish function for all items in the array to generate markdown.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 import inquirer from 'inquirer';
 import fs, { write } from 'fs';
 import generateMarkdown from './utils/generateMarkdown.js';
-// import { type } from 'os';
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -21,47 +20,47 @@ const questions = [
 //     name: "screenshot",
 //     message: "Please enter a screenshot of your project using the relative path."
 // },
-// {
-//     type: "input",
-//     name: "Installation",
-//     message: "Provide a step-by-step description of how to get the development environment running."
-// },
-// {
-//     type: "input",  
-//     name: "usage",
-//     message: "Provide instructions and examples for use, including screenshots."
-// },
-// {
-//     type: "input",
-//     name: "credits",
-//     message: "List your collaborators, if any, with links to their GitHub profiles."
-// },
+{
+    type: "input",
+    name: "installation",
+    message: "Provide a step-by-step description of how to get the development environment running."
+},
+{
+    type: "input",  
+    name: "usage",
+    message: "Provide instructions and examples for use, including screenshots."
+},
+{
+    type: "input",
+    name: "credits",
+    message: "List your collaborators, if any, with links to their GitHub profiles."
+},
 {
     type: "list",
     name: "license",
     message: "Please enter the license for your project.",
     choices: ["MIT", "GNU", "Apache"]
 },
-// {
-//     type: "input",
-//     name: "badges",
-//     message: "Please enter any badges you would like to include."
-// },
-// {
-//     type: "input",
-//     name: "features",
-//     message: "Do you have lots of features? Add them here!"
-// },
-// {
-//     type: "input",
-//     name: "contributing",
-//     message: "Would you like others to contribute to your project? Tell them how!"
-// },
-// {
-//     type: "input",
-//     name: "tests",
-//     message: "Please enter the tests for your project, with applicaple tests."
-// },
+{
+    type: "input",
+    name: "badges",
+    message: "Please enter any badges you would like to include."
+},
+{
+    type: "input",
+    name: "features",
+    message: "Do you have lots of features? Add them here!"
+},
+{
+    type: "input",
+    name: "contributing",
+    message: "Would you like others to contribute to your project? Tell them how!"
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Please enter the tests for your project, with applicaple tests."
+},
 ];
 
 // TODO: Create a function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {
+// }
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
 function renderLicenseBadge(license) {
   const licensebadges = {
     'MIT': '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)',
@@ -12,28 +20,45 @@ function renderLicenseBadge(license) {
     return '';
   }
 }
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {
 
-// }
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # Title
-  ${data.title}
+  return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
-  # Description
+  ## Description
   ${data.description}
 
-  # License
-  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [Badges](#badges)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+  ## Badges
+  ${data.badges}
+  
+  ## Features
+  ${data.features}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
   `;
-}
+
+
+};
 
 export default generateMarkdown;
